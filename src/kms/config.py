41d0cbfbbae89d681ffd1df2fcc50ab2498e0fce
Johnny Hyytiäinen <Johnny.Hyytiainen@gmail.com>
2026-07-06 17:52:47 +0200

    feat(S3 bucket name): added s3 buckets name in settings baseclass

--- src/kms/config.py
+++ src/kms/config.py
@@ -23,6 +23,7 @@ class Settings(BaseSettings):
     AWS_ACCESS_KEY_ID: str
     AWS_SECRET_ACCESS_KEY: str
     AWS_ENDPOINT_URL: str
+    S3_BUCKET_NAME: str = "kms-course-material"  # Eget default värde, min buckets namn är ej secret och bör bo hemma i min Settings class
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
