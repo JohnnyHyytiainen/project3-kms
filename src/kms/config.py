e7def9d7b104d767cbec8c5a39f89d67d1f18896
Johnny Hyytiäinen <Johnny.Hyytiainen@gmail.com>
2026-07-06 16:52:44 +0200

    refactor(constants): updated pydantic Settings class to contain AWS constants for S3 LocalStack

--- src/kms/config.py
+++ src/kms/config.py
@@ -20,6 +20,10 @@ class Settings(BaseSettings):
 
     GRUNDEN_API_KEY: str  # oanvänd förrän MVP v3
 
+    AWS_ACCESS_KEY_ID: str
+    AWS_SECRET_ACCESS_KEY: str
+    AWS_ENDPOINT_URL: str
+
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
     @property
